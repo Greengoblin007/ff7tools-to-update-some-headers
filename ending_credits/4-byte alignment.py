78f02745aaaf3175b02f4d5de9e994b7cecf1e33
Greengoblin007 <71230597+Greengoblin007@users.noreply.github.com>
2022-12-31 13:18:53 +0100

    Create 4-byte alignment.py

---
+++ ending_credits/4-byte alignment.py
@@ -0,0 +1,531 @@
+# 4-byte alignment for STAFF2.BIN files
+
+########## File 00 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_00.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_00.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+########## File 01 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_01.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_01.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+########## File 02 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_02.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_02.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+########## File 03 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_03.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_03.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+########## File 04 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_04.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_04.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+########## File 05 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_05.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_05.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+########## File 06 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_06.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_06.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+########## File 07 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_07.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_07.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+########## File 08 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_08.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_08.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+########## File 09 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_09.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_09.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+########## File 10 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_10.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_10.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+########## File 11 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_11.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_11.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+########## File 12 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_12.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_12.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+########## File 13 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_13.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_13.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+########## File 14 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_14.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_14.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+########## File 15 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_15.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_15.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+########## File 16 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_16.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_16.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+########## File 17 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_17.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_17.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+########## File 18 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_18.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_18.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+########## File 19 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_19.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_19.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+########## File 20 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_20.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_20.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+########## File 43 ##########
+
+# Open the file in binary mode
+f = open('STAFF2_43.LZS', 'rb')
+
+# Read the entire file into memory
+data = f.read()
+
+# Go to first position
+f.seek(0)
+
+# Calculate the number of bytes needed to align the data to 4-byte boundaries
+alignment = (4 - (len(data) % 4)) % 4
+
+# Add padding to the end of the data to ensure 4-byte alignment
+data += b'\x00' * alignment
+
+# Write the aligned data back to the file
+with open('STAFF2_43.LZS', 'wb') as f:
+    f.write(data)
+
+# Close the file
+f.close()
+
+# End of file
