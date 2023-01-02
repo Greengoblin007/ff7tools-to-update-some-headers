6575dd68b0655507e657802d10d6df4d93c39018
Greengoblin007 <71230597+Greengoblin007@users.noreply.github.com>
2023-01-02 10:54:32 +0100

    Add files via upload

--- battle/headers.py
+++ battle/headers.py
@@ -13,25 +13,25 @@ f.seek(0)
 
 ########## Compressed file ##########
 
-# Get the size of compressed file 0_0 in bytes
-compressed_file_00_size = os.path.getsize("CO_1_0.GZ")
+# Get the size of compressed file 1_0 in bytes
+compressed_file_10_size = os.path.getsize("CO_1_0.GZ")
 
 # Show it as a hex string (little-endian)
-compressed_little_endian_00 = compressed_file_00_size.to_bytes(2, 'little').hex().upper()
+compressed_little_endian_10 = compressed_file_10_size.to_bytes(2, 'little').hex().upper()
 
 # Write to the file
-f.write(binascii.unhexlify(compressed_little_endian_00))
+f.write(binascii.unhexlify(compressed_little_endian_10))
 
 ########## Uncompressed file ##########
 
-# Get the size of uncompressed file 00 in bytes
-uncompressed_file_00_size = os.path.getsize("CO_1_0.data")
+# Get the size of uncompressed file 1_0 in bytes
+uncompressed_file_10_size = os.path.getsize("CO_1_0.data")
 
 # Show it as a hex string (little-endian)
-uncompressed_little_endian_00 = uncompressed_file_00_size.to_bytes(2, 'little').hex().upper()
+uncompressed_little_endian_10 = uncompressed_file_10_size.to_bytes(2, 'little').hex().upper()
 
 # Write to the file
-f.write(binascii.unhexlify(uncompressed_little_endian_00))
+f.write(binascii.unhexlify(uncompressed_little_endian_10))
 
 ########## Append to the file ##########
 
@@ -51,25 +51,25 @@ f.seek(0)
 
 ########## Compressed file ##########
 
-# Get the size of compressed file 1_0 in bytes
-compressed_file_10_size = os.path.getsize("CO_0_0.GZ")
+# Get the size of compressed file 0_0 in bytes
+compressed_file_00_size = os.path.getsize("CO_0_0.GZ")
 
 # Show it as a hex string (little-endian)
-compressed_little_endian_10 = compressed_file_10_size.to_bytes(2, 'little').hex().upper()
+compressed_little_endian_00 = compressed_file_00_size.to_bytes(2, 'little').hex().upper()
 
 # Write to the file
-f.write(binascii.unhexlify(compressed_little_endian_10))
+f.write(binascii.unhexlify(compressed_little_endian_00))
 
 ########## Uncompressed file ##########
 
-# Get the size of uncompressed file 1_0 in bytes
-uncompressed_file_10_size = os.path.getsize("CO_0_0.data")
+# Get the size of uncompressed file 0_0 in bytes
+uncompressed_file_00_size = os.path.getsize("CO_0_0.data")
 
 # Show it as a hex string (little-endian)
-uncompressed_little_endian_10 = uncompressed_file_10_size.to_bytes(2, 'little').hex().upper()
+uncompressed_little_endian_00 = uncompressed_file_00_size.to_bytes(2, 'little').hex().upper()
 
 # Write to the file
-f.write(binascii.unhexlify(uncompressed_little_endian_10))
+f.write(binascii.unhexlify(uncompressed_little_endian_00))
 
 ########## Append to the file ##########
 
