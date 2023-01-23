2415dad5253a91181e20fe8fe750a600354206b4
Greengoblin007 <71230597+Greengoblin007@users.noreply.github.com>
2023-01-23 14:38:38 +0100

    Update headers.py

--- battle/headers.py
+++ battle/headers.py
@@ -5,7 +5,7 @@ import binascii
 
 ########## HEADER1 ##########
 
-# Open the file in read/write mode
+# Open the file in write binary mode
 f = open('HEADER1.BIN', 'wb')
 
 # Go to first position
@@ -19,6 +19,8 @@ compressed_file_10_size = os.path.getsize("CO_1_0.GZ")
 # Show it as an hex string (little-endian)
 compressed_little_endian_10 = compressed_file_10_size.to_bytes(2, 'little').hex().upper()
 
+print (compressed_little_endian_10)
+
 # Write to the file
 f.write(binascii.unhexlify(compressed_little_endian_10))
 
@@ -29,6 +31,8 @@ uncompressed_file_10_size = os.path.getsize("CO_1_0.data")
 
 # Show it as an hex string (little-endian)
 uncompressed_little_endian_10 = uncompressed_file_10_size.to_bytes(2, 'little').hex().upper()
+
+print(uncompressed_little_endian_10)
 
 # Write to the file
 f.write(binascii.unhexlify(uncompressed_little_endian_10))
@@ -43,7 +47,7 @@ f.close()
 
 ########## HEADER2 ##########
 
-# Open the file in read/write mode
+# Open the file in write binary mode
 f = open('HEADER2.BIN', 'wb')
 
 # Go to first position
@@ -57,6 +61,8 @@ compressed_file_00_size = os.path.getsize("CO_0_0.GZ")
 # Show it as an hex string (little-endian)
 compressed_little_endian_00 = compressed_file_00_size.to_bytes(2, 'little').hex().upper()
 
+print (compressed_little_endian_00)
+
 # Write to the file
 f.write(binascii.unhexlify(compressed_little_endian_00))
 
@@ -67,6 +73,8 @@ uncompressed_file_00_size = os.path.getsize("CO_0_0.data")
 
 # Show it as an hex string (little-endian)
 uncompressed_little_endian_00 = uncompressed_file_00_size.to_bytes(2, 'little').hex().upper()
+
+print (uncompressed_little_endian_00)
 
 # Write to the file
 f.write(binascii.unhexlify(uncompressed_little_endian_00))
