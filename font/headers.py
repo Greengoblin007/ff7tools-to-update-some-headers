152d1e5a1ac69a76b2d4c89c71f7a18431ad215e
Greengoblin007 <71230597+Greengoblin007@users.noreply.github.com>
2023-01-23 14:40:51 +0100

    Update headers.py

--- font/headers.py
+++ font/headers.py
@@ -5,7 +5,7 @@ import binascii
 
 ########## HEADER1 ##########
 
-# Open the file in read/write mode
+# Open the file in write binary mode
 f = open('HEADER1.BIN', 'wb')
 
 # Go to first position
@@ -19,6 +19,8 @@ compressed_file_00_size = os.path.getsize("WINDOW_0_0.GZ")
 # Show it as an hex string (little-endian)
 compressed_little_endian_00 = compressed_file_00_size.to_bytes(2, 'little').hex().upper()
 
+print (compressed_little_endian_00)
+
 # Write to the file
 f.write(binascii.unhexlify(compressed_little_endian_00))
 
@@ -29,6 +31,8 @@ uncompressed_file_00_size = os.path.getsize("WINDOW_0_0.data")
 
 # Show it as an hex string (little-endian)
 uncompressed_little_endian_00 = uncompressed_file_00_size.to_bytes(2, 'little').hex().upper()
+
+print (uncompressed_little_endian_00)
 
 # Write to the file
 f.write(binascii.unhexlify(uncompressed_little_endian_00))
@@ -43,7 +47,7 @@ f.close()
 
 ########## HEADER2 ##########
 
-# Open the file in read/write mode
+# Open the file in write binary mode
 f = open('HEADER2.BIN', 'wb')
 
 # Go to first position
@@ -57,6 +61,8 @@ compressed_file_01_size = os.path.getsize("WINDOW_0_1.GZ")
 # Show it as an hex string (little-endian)
 compressed_little_endian_01 = compressed_file_01_size.to_bytes(2, 'little').hex().upper()
 
+print (compressed_little_endian_01)
+
 # Write to the file
 f.write(binascii.unhexlify(compressed_little_endian_01))
 
@@ -67,6 +73,8 @@ uncompressed_file_01_size = os.path.getsize("WINDOW_0_1.data")
 
 # Show it as an hex string (little-endian)
 uncompressed_little_endian_01 = uncompressed_file_01_size.to_bytes(2, 'little').hex().upper()
+
+print (uncompressed_little_endian_01)
 
 # Write to the file
 f.write(binascii.unhexlify(uncompressed_little_endian_01))
@@ -81,7 +89,7 @@ f.close()
 
 ########## HEADER3 ##########
 
-# Open the file in read/write mode
+# Open the file in write binary mode
 f = open('HEADER3.BIN', 'wb')
 
 # Go to first position
@@ -95,6 +103,8 @@ compressed_file_10_size = os.path.getsize("WINDOW_1_0.GZ")
 # Show it as an hex string (little-endian)
 compressed_little_endian_10 = compressed_file_10_size.to_bytes(2, 'little').hex().upper()
 
+print (compressed_little_endian_10)
+
 # Write to the file
 f.write(binascii.unhexlify(compressed_little_endian_10))
 
@@ -105,6 +115,8 @@ uncompressed_file_10_size = os.path.getsize("WINDOW_1_0.data")
 
 # Show it as an hex string (little-endian)
 uncompressed_little_endian_10 = uncompressed_file_10_size.to_bytes(2, 'little').hex().upper()
+
+print (uncompressed_little_endian_10)
 
 # Write to the file
 f.write(binascii.unhexlify(uncompressed_little_endian_10))
